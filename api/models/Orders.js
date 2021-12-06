559549be65d3c9d9c7131c5fe5d1d543950f13ac
Ankith <ankithpal721@gmail.com>
2021-12-06 21:20:14 +0530

    second commit to client

--- api/models/Orders.js
+++ api/models/Orders.js
@@ -15,6 +15,10 @@ const OrdersSchema = new mongoose.Schema({
     amount:{
         type:String,
         required:true
+    },
+    status:{
+        type:String,
+        default:"placed"
     }
 },{timestamps:true})
 
--- api/routes/order.js
+++ api/routes/order.js
@@ -19,16 +19,44 @@ router.post('/order', async (req,res)=>{
     }
 })
 
+router.put('/update/:id',async (req,res)=>{
+    try{
+         const updated = await Orders.findByIdAndUpdate(req.params.id,{status:req.body.status},{new:true});
+        
+         res.status(200).json(updated.status);
+    }catch(err){
+        res.status(200).json(err);
+    }
+})
+
 router.get('/:id',async(req,res)=>{
     try{
             const user = await Users.findById(req.params.id);
               const  email=user.email;
-            const userOrder = await Orders.find({email});
+            const userOrder = await Orders.find({email}).sort({createdAt:-1});
             res.status(200).json(userOrder);
     }catch(err){
         res.status(500).json(err);
     }
 })
+router.get('/singleorder/:id',async(req,res)=>{
+    try{
+            const user = await Orders.findById(req.params.id);
+            res.status(200).json(user);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+router.get('/',async(req,res)=>{
+    try{
+         const orders= await Orders.find().sort({createdAt:-1});
+         res.status(200).json(orders);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
 
 
 
